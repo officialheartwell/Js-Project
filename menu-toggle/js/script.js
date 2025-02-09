84a6a54fe6c8c02a670bc0ff0747de7300daabf1
officialheartwell <okorieheartwell@gmail.com>
2025-02-09 01:18:58 +0100

    finish working on the toggle Navigating menue for mobile, tablet and desktop

--- menu-toggle/js/script.js
+++ menu-toggle/js/script.js
@@ -3,9 +3,8 @@ const hamburgerMenu = document.querySelector(".hamburger-menu");
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 
-
 hamburgerMenu.addEventListener("click", () => {
   menu.classList.toggle("hidden");
-  hamburger.classList.toggle("toggle");
+  hamburgerMenu.classList.toggle("toggle");
   console.log(menu, hamburger);
 });
